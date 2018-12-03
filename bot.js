aecd2ab895f09f2f407fc1ac4463e81cbe68a120
hghsgdjhugkf <45543772+hghsgdjhugkf@users.noreply.github.com>
2018-12-03 13:47:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`سبحان الله`,"http://twitch.tv/YouTubeee")
 client.user.setStatus("dnd")
 });
 
